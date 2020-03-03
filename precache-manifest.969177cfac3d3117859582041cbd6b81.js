6ed3c8d2bcd62ffd23416ea0c24ac4497003e9bd
watercrawler <watercrawler@naver.com>
2020-03-03 18:19:24 +0900

    Updates

---
+++ precache-manifest.969177cfac3d3117859582041cbd6b81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e158a2b0a417dcdd0f92ddc8cec197c",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "c0cae338b32585ba8f31",
+    "url": "/movie_app/static/css/main.c4f3e8a5.chunk.css"
+  },
+  {
+    "revision": "79b3d98d9236aa532655",
+    "url": "/movie_app/static/js/2.3aa1e297.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app/static/js/2.3aa1e297.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0cae338b32585ba8f31",
+    "url": "/movie_app/static/js/main.4c7572d6.chunk.js"
+  },
+  {
+    "revision": "c0057b52e72b0f614c37",
+    "url": "/movie_app/static/js/runtime-main.15be7292.js"
+  }
+]);
